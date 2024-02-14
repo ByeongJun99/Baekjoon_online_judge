4eb57332df30c9be44ba1a4b302ca46f7e22c9e8
ByeongJun <81874336+ByeongJun99@users.noreply.github.com>
2024-02-14 12:33:59 +0900

    [Silver III] Title: N과 M (2), Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,12 @@
+import sys
+from itertools import combinations
+
+n, m = map(int, sys.stdin.readline().split())
+arr = [i for i in range(1, n+1)]
+
+answer = list(combinations(arr, m))
+
+for i in answer:
+    for j in i:
+        print(j, end=" ")
+    print()
